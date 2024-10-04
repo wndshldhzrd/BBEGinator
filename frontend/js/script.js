6e09e8481957f37de9fb60f42acfdff8d6f69e70
James Baker <jamesjbaker@tutanota.com>
2024-10-04 16:55:48 -0400

    Complete getHP and getModifier in script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -10,26 +10,58 @@ function getJSON() {
 // Button that creates a monster (TEMP)
 const createMonsterButton = document.querySelector("#create-monster");
 
+function createPar (text) {
+    const par = document.createElement("p");
+    par.textContent = text;
+    return par;
+}
+
+function getModifier (score) {
+    console.log(`${Math.floor((score - 10) / 2)}`);
+    return Math.floor((score - 10) / 2);
+}
+
+function getHP (monsterSize, numDice, constMod) {
+    switch (monsterSize) {
+        case "tiny":
+            return `${Math.floor(2.5 * numDice) + constMod} (${numDice}d4)`;
+        case "small":
+            return `${Math.floor(3.5 * numDice) + constMod} (${numDice}d6)`;
+        case "medium":
+            return `${Math.floor(4.5 * numDice) + constMod} (${numDice}d8)`;
+        case "large":
+            return `${Math.floor(5.5 * numDice) + constMod} (${numDice}d10)`;
+        case "huge":
+            return `${Math.floor(6.5 * numDice) + constMod} (${numDice}d12)`;
+        case "gargantuan":
+            return `${Math.floor(10.5 * numDice) + constMod} (${numDice}d20)`;
+        default:
+            return `Error`;
+    }
+}
+
 function loadMonster (monster) {
     // Div where we will display monsters
     const showMonsterDiv = document.querySelector(".monster-display");
     showMonsterDiv.innerHTML = "";
 
     // Adding name to first line
-    const name = document.createElement("p");
-    name.textContent = monster.name;
-
+    showMonsterDiv.appendChild(createPar(monster.name));
     // Adding size, type, and alignment
-    const description = document.createElement("p");
-    description.textContent = monster.size + " " + monster.type + ", " + monster.alignment;
-
+    showMonsterDiv.appendChild(createPar(monster.size + " " + monster.type + ", " + monster.alignment));
     // Line break
-    const lbreak = document.createElement("hr");
-    // lbreak.textContent = "-------";
-
-    showMonsterDiv.appendChild(name);
-    showMonsterDiv.appendChild(description);
-    showMonsterDiv.appendChild(lbreak);
+    showMonsterDiv.appendChild(document.createElement("hr"));
+    // Armor class
+    showMonsterDiv.appendChild(createPar("Armor Class " + monster.otherArmorDesc));
+    // Hit Points
+    const maybeHP = monster.hpText;
+    const constMod = getModifier(monster.conPoints);
+    if (/^\d+$/.test(maybeHP)) {
+        showMonsterDiv.appendChild(createPar("Hit Points " + getHP(monster.size, maybeHP, constMod)));
+    } else {
+        showMonsterDiv.appendChild(createPar("Hit Points " + monster.hpText));
+    }
+    // Speed
 }
 
 // TODO: Catch invalid json/timeouts so we don't have errors on loadMonster
