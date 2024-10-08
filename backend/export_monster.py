2c37110d9c9d302a002bc79fdd6309243ec6c6b1
voronv27 <152320775+voronv27@users.noreply.github.com>
2024-10-08 17:26:43 -0400

    Added more conversions to export_monster
    Fixed speed conversions, added hover, and changed cr to a string

--- backend/export_monster.py
+++ backend/export_monster.py
@@ -48,22 +48,36 @@ def export(monster):
 	for s in senses:
 		jsonData[s] = get_sense(s, jsonData[s])
 
+	#blind boolean
 	jsonData["blind"] = (jsonData["blind"].find("blind ") != -1)
-	speeds = ["speed", "flySpeed", "swimSpeed", "climbSpeed", "burrowSpeed"] #note: speed = walk
-	speedsActual = ["walk, fly, swim, climb, burrow"]
-	for s in speeds: #dear vicky: climb does not work please help :(
+
+	#walk speed
+	jsonData["speed"] = jsonData["speed"]["walk"]
+
+	#other speeds
+	speeds = ["flySpeed", "swimSpeed", "climbSpeed", "burrowSpeed"] #note: speed = walk
+	speedsActual = ["fly", "swim", "climb", "burrow"]
+	for i in range(0, len(speeds)): #dear vicky: climb does not work please help :(
+		s = speeds[i]
+		s_actual = speedsActual[i]
 		print(s)
 		print(jsonData[s])
-		if(s == "speed"):
-			jsonData[s] = jsonData[s]["walk"]
-		else:
-			if(jsonData[s][s[:s.find("Speed")]] != 0):
-				jsonData[s] = 0;
-			else:
-				jsonData[s] = jsonData[s][s[:s.find("Speed")]]
 
-	jsonData["blind"] = (jsonData["blind"].find("blind ") != -1)
-	jsonData["isLegendary"] = len(jsonData["isLegendary"]) > 0
+		if(s_actual in jsonData[s]):
+			jsonData[s] = jsonData[s][s_actual]
+		else:
+			jsonData[s] = 0
+
+	#hover boolean
+	jsonData["hover"] = "hover" in jsonData["hover"]
+
+	#convert cr from double to string of an int
+	jsonData["cr"] = str(int(jsonData["cr"]))
+
+	#isLegendary
+	jsonData["isLegendary"] = jsonData["isLegendary"] == "" or jsonData["isLegendary"] == None
+
+	#armor
 	armor_str = jsonData["otherArmorDesc"].strip()
 	print(armor_str)
 	a_index = armor_str.find(" ")
@@ -116,7 +130,7 @@ def export(monster):
 
 #for testing purposes
 if __name__ == "__main__":
-	monSlug = "accursed-guardian-naga-a5e"
+	monSlug = "aboleth-nihilith"
 	print("This is a test to convert a monster from the JSON file " +
 	 "format we get from mgetter.py to a .monster file")
 	print("Current monster slug: " + monSlug + "\n")
