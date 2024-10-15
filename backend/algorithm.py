e8a6316b2ee1bd978103da438273ab09577e46b8
south-dakota <liggea@rpi.edu>
2024-10-15 16:27:53 -0400

    modes

--- backend/algorithm.py
+++ backend/algorithm.py
@@ -16,7 +16,7 @@ class EncounterType(Enum):
     # BULLSHIT
 
     #parses each monster from the mgetter and calulates its dmg/health ranges
-def Algorithm(party, difficulty, monsterList, lair, guys):
+def Algorithm(party, difficulty, monsterList, lair, guys, mode):
     
     points = 0
     monsters = []
@@ -40,20 +40,41 @@ def Algorithm(party, difficulty, monsterList, lair, guys):
     points = (totalDmg+totalHealth+ (partyDmgMaxAvg/4)) * (float(difficulty.value) * (3 / 2))      #calculation for the point pool
     print(points)
     print(f"I have {points} points to spend")
-    for i in range(guys):
-        print(f"Generating monster {i + 1} of {guys}")
-        toSpend = 0
-        if(i < guys - 1):
-            toSpend = randint(int(points * 0.1), int(points * 0.67))
-            print(f"I want to spend {toSpend} points")
-            points -= toSpend
-        else:
-            print(f"This is the last monster, using my last {points} points")
-    
+    if(mode == "random"):
+        for i in range(guys):
+            print(f"Generating monster {i + 1} of {guys}")
+            toSpend = 0
+            if(i < guys - 1):
+                toSpend = randint(int(points * 0.1), int(points * 0.67))
+                print(f"I want to spend {toSpend} points")
+                points -= toSpend
+            else:
+                print(f"This is the last monster, using my last {points} points")
+    elif(mode == "sameMonster"):
+        toSpend = points / guys
+        print(f"I want to buy {guys} of the same monster with a point value around {toSpend}")
+    elif(mode == "balanced"):
+        toSpend = points / guys
+        print(f"I want to buy {guys} different monsters with point values around {toSpend}")
+    elif(mode == "boss"):
+        print(f"This is a boss encounter, I will spend most of my points on a boss")
+        toSpend = randint(int(points * 0.67), int(points * 0.8))
+        print(f"Generating monster 1 of {guys}")
+        print(f"I want to spend {toSpend} points on the boss")
+        points -= toSpend
+        for i in range(guys - 1):
+            print(f"Generating monster {i + 2} of {guys}")
+            toSpend = 0
+            if(i < guys - 2):
+                toSpend = randint(int(points * 0.1), int(points * 0.67))
+                print(f"I want to spend {toSpend} points")
+                points -= toSpend
+            else:
+                print(f"This is the last monster, using my last {points} points")
 
 jimmy = PartyMember(65, "shmorbler", 7)            #testing values
 glorth = PartyMember(50, "glunkman", 13)
 party = [jimmy, glorth]
 
 georgeBush = Monster("George W. Bush", "george", 45, 300, 5, 7)
-Algorithm(party, EncounterType.MEDIUM, [georgeBush], None, 6)
+Algorithm(party, EncounterType.MEDIUM, [georgeBush], None, 4, "boss")
