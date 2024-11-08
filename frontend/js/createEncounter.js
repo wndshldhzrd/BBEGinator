f3059d59b8c6d703482d76c532b7b3ebee8c8c1f
TheFireDragoon <88166643+TheFireDragoon@users.noreply.github.com>
2024-11-08 16:47:43 -0500

    CreateEncounter - removing unnecessary stuff
    Updating the createEncounter.html and .js to remove unnecessary buttons + their associated code since we implemented it in a different way than we initially planned for

--- frontend/js/createEncounter.js
+++ frontend/js/createEncounter.js
@@ -1,6 +1,3 @@
-//variable which keeps track of how many monsters the page is currently displaying
-var monsters = [];
-
 //upload files event listener and behavior upon receiving a submit
 const form = document.querySelector('form');
 form.addEventListener('submit', handleSubmit);
@@ -92,64 +89,5 @@ function getFileContents(fileList) {
             data.innerHTML += fr.result;
         }
         fr.readAsText(f);
-    }
-}
-
-//function for adding a monster to the createEncounter page--we should remove this
-function addMonster() {
-
-    //getting the div which monsters are added to
-    var monsterStatBlockDisplay = document.getElementById("monsterStatBlocks");
-
-    //creating our monster stat block to be added to the page
-    var monsterStatBlock = document.createElement("div");
-
-    //adding our monster element to the list of monster elements
-    monsters.push(monsterStatBlock)
-
-    //setting the inner html of the monster statblock
-    //ideally refactor into an html template page, but good enough for now
-    monsterStatBlock.innerHTML =
-    `<div class="monsterInput">
-        <label class="monsterName">Monster ${monsters.length}</label><br>
-        <hr>
-        UploadMonster <button onclick="uploadMonster();">Upload Monster</button>
-        <br>
-        Level <input class="stat-Input" type="number" min="1" value="1">
-        <br>
-        Hp <input class="stat-Input" id="monsterHealth" type="number" min="1" value="1">
-    </div>`;
-
-    //displaying our monster on the page
-    monsterStatBlockDisplay.appendChild(monsterStatBlock);
-
-    toggleRemoveMonsterButton();
-}
-
-function uploadMonster() {
-    
-}
-
-function removeMonster() {
-
-    //grabbing all of our monster stat blocks
-    var monsterStatBlocks = document.getElementsByClassName("monsterInput");
-
-    //removing the last one
-    monsterStatBlocks[monsters.length-1].remove();
-
-    //removing the monster element from our array
-    monsters.pop();
-
-    toggleRemoveMonsterButton();
-}
-
-//determining whether to hide/show our remove monster button
-function toggleRemoveMonsterButton() {
-    if (monsters.length > 1) {
-        document.getElementById("removeMonster").style.display= "inline-block";
-    }
-    else {
-        document.getElementById("removeMonster").style.display= "none";
     }
 }
