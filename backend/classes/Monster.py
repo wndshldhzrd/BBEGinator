7c5c1a341cd6727b707fba5de01cce035512491f
south-dakota <liggea@rpi.edu>
2024-11-08 16:57:24 -0500

    a lot

--- backend/classes/Monster.py
+++ backend/classes/Monster.py
@@ -1,6 +1,12 @@
-class Monster():
-    
+def WordToNum(word):
+    if word == "one": return 1
+    elif word == "two": return 2
+    elif word == "three": return 3
+    elif word == "four": return 4
+    elif word == "five": return 5
+    else: return -69420
 
+class Monster():
     def __init__(self, slug, name, ac, hp, speeds, stats, saves, vulnerabilities, resistances, immunities, actions, abilities, cr):
         self.slug = slug
         self.name = name
@@ -16,8 +22,9 @@ class Monster():
         self.abilities = abilities
         self.cr = cr
         self.points = 0
-
-        points += hp
+        
+        self.points += hp
+        if (ac >= 10):self.points = int( float(self.points) * (1 + (ac-10)/10))
 
         #proficiency calc
         if (self.cr >=29):prof = 9
@@ -31,65 +38,62 @@ class Monster():
 
         #stat & save point calculation
         Saves = {}
-        statCalc = stats.keys
+        statCalc = stats.keys()
         for stat in statCalc:   #for each stat
             if(stats[stat]) > 10:
-                points += (stats[stat]//2 * 5)  #if the stat is greater than 10, then add to points
+                self.points += ((stats[stat] - 10)//2 * 5)  #if the stat is greater than 10, then add to points
             for SaveProficiency in saves:
                 if (stats[stat] == SaveProficiency):Saves[stat] = stats[stat]//2 + prof # for each stat, find if its proficient in that save and then add respectively
                 else:Saves[stat] = stats[stat]//2
 
+        dmgAvg = 0
+        dmgMax = 0
 
-        if (ac >= 10):points = int( float(points) * (1 + (ac-10)/10))
         #multiattack calculation
         attacks = []
         for x in actions:
-            if x.name == "multiattack":
-                x.description.split()
-                for position in range(len(x.description)):
-                    if x.description(position) == "one" or x.description(position) == "two" or x.description(position) == "three" or x.description(position) == "four" or x.description(position) == "five":
-                        if x.description(position+1) == "with":
-                            attacks.append(x.description(position), x.description(position+3))
-            break
+            if x["name"] == "multiattack":
+                desc = x["description"].split()
+                for position in range(len(desc)):
+                    if desc[position] == "one" or desc[position] == "two" or desc[position] == "three" or desc[position] == "four" or desc[position] == "five":
+                        if desc[position + 1] == "with":
+                            for i in range(WordToNum(desc[position])): attacks.append(desc[position], desc[position + 3])
+        if(len(attacks) > 0): # multiattack
+            for action in actions:
+                #if saving throw then pase as is
+                #else do this
+                for attack in attacks:
+                    if action["name"].lower() == attack[1]:
+                        dice = action["damage_dice"].split('d')
+                        bonus = action["damage_bonus"]
+                        dmgMax = dice[0] * dice[1] + bonus
+                        avgRoll = (dice[1] - 1) / 2
+                        dmgAvg = dice[0] * avgRoll + bonus
 
-        dmgAvg = 0
-        dmgMax = 0
-        for action in actions:
-            #if saving throw then pase as is
-            #else do this
-            for attack in attacks:
-               if action["name"].lower() == attack[1]:
-                   dice = action["damage_dice"].split('d')
-                   bonus = action["damage_bonus"]
-                   dmgMax = dice[0] * dice[1] + bonus
-                   avgRoll = (dice[1] - 1) / 2
-                   dmgAvg = dice[0] * avgRoll + bonus
-
-
-        #vulnerailities
-            #alex side
-            #same as others
-
-        #resistancesr
-            #alex side
-
-        #invulnerabilities
-            #alex side
+        self.points += dmgAvg + dmgMax
         
-
-                   
-        resistances.split(';')
-        if len(resistances) > 1:
-            for res in resistances:
-                res.split(',')
-                for r in res:
-                    points += 10
+        joe = vulnerabilities.split(';')
+        if len(joe) >= 1:
+            for vul in joe:
+                joe2 = vul.split(',')
+                for v in joe2:
+                    self.points *= 0.9
+     
+        jimmy = resistances.split(';')
+        print(jimmy)
+        if len(jimmy) >= 1:
+            for res in jimmy:
+                jimmy2 = res.split(',')
+                for r in jimmy2:
+                    self.points *= 1.1
+                    
         
-        immunities.split(';')
-        if len(immunities) > 1:
-            for imm in immunities:
-                imm.split(',')
-                for i in imm:
-                    points += 10
-                   
-        points += dmgAvg + dmgMax
+        john = immunities.split(';')
+        if len(john) >= 1:
+            for imm in john:
+                john2 = imm.split(',')
+                for i in john2:
+                    self.points *= 1.1
+                    
+        if(self.points < 1): self.points = 1
+        self.points = int(self.points)
