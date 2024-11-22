7839ff80210b356f4b32ada1b1ff7eece7a76e90
TheFireDragoon <88166643+TheFireDragoon@users.noreply.github.com>
2024-11-22 16:19:47 -0500

    search.js attempted filter addition
    Added code that should hopefully allow for search.js's search monster function to actually be affected by some of the search filters we have set up.

--- frontend/js/search.js
+++ frontend/js/search.js
@@ -123,9 +123,59 @@ let data = null;
 let dataIndex = 0;
 
 export async function testRunningPython() {
+    payload = {
+        //most comments copy/pasted from mgetter
+    'slug__in': '', //allows you to search for specific slugs, can do multiple at a time but formatting awkward
+    'slug__iexact': '', //allows you to search for 1 slug specifically
+    'slug': '', //tbh idk dfference between this and prev
+    'name__iexact': '', //specific name
+    'name': '', //SPECIFIC NAME
+    'name__icontains': '', //allows you to search for creatures w names that contain this string. example is dragon will return al dragons
+    'desc__iexact': '', //exact description
+    'desc': '', //idk difference between this and prev
+    'desc__in': '', //unsure rn
+    'desc__icontains': '', //unsure
+    'cr': '', //exact cr
+    'cr__range': '', //range for cr but annoying to do so i implement later
+    'cr__gt': '', //number you want cr to be greater than
+    'cr__gte': '', //number u want cr to be greater than or equal to
+    'cr__lt': '', //number u want cr to be less than
+    'cr__lte': '', //number u want cr to be less than or equal to
+    'hit_points': '', //hit points of creature
+    'hit_points__range': '', //range for hp but annoying to do so i implement later
+    'hit_points__gt': '', //number u want hp to be greater than
+    'hit_points__gte': document.getElementById("hpMax").value, //max hp
+    'hit_points__lt': '', //number u want hp to be less than
+    'hit_points__lte': document.getElementById("hpMin").value, //min hp
+    'armor_class': '', //exact ac
+    'armor_class__range': '', //range for ac but annoying to do so i implement later
+    'armor_class__gt': '', //number u want ac to be greater than
+    'armor_class__gte': document.getElementById("acMax").value, //max ac
+    'armor_class__lt': '', //number u want ac to be less than
+    'armor_class__lte': document.getElementById("acMin").value, //min ac
+    'type__iexact': document.getElementById("type-dropdown").value,  //creature type
+    'type': '', //unsure of difference between ^
+    'type__in': '', //disregard
+    'type__icontains': '', //allows you to search w a term and have results be types that contain that term
+    'size__iexact': document.getElementById("size-dropdown").value, //creature size
+    'size': '', //unsure of difference between^
+    'size__in': '', //unsure
+    'size__icontains': '', //allows you to search w a term and have results be sizes that contain that term, not sure why ud want this
+    'page_no': '', //specific page #
+    'page_no__range': '', //annoying, alr explained this 100x
+    'page_no__gt': '', //page number greater than
+    'page_no__gte': '', //page number greater than or equal to
+    'page_no__lt': '', //page number less than
+    'page_no__lte': '', //page number less than or equal to
+    //all this document slug stuff is for what compendium the content is from, not sure if we will ever really be using
+    'document__slug__iexact': '', 
+    'document__slug': '',
+    'document__slug__in': '',
+    'document__slug__not_in': ''
+    }
     testMessage.textContent = "Button clicked, awaiting result... ";
     testMessage2.textcontent = "";
-    const url = "http://localhost:5000/test-script";
+    const url = "http://localhost:5000/test-script" + JSON.stringify(payload);
 
     console.log("fetching");
     test();
